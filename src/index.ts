ad71d295df75d0adb0247e775271d5181550f2f8
Derrick Hammer <derrick@derrickhammer.com>
2023-02-20 17:35:54 -0500

    *Update module hash

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Client, factory } from "@lumeweb/libkernel-universal";
 
-const MODULE = "AAAz2qQHwKQ7YurprbLmzpHc66BY0pljDZsa_a6QmeBe_g";
+const MODULE = "AAAArUFfECicfHF6nB8bTOVPlIH5zLOrT0nviQocrWmohg";
 
 export interface Response {
   id?: number;
