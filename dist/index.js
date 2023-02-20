6db2285a152eefdcf1c75448cba98d1c7023f59e
Derrick Hammer <derrick@derrickhammer.com>
2023-02-20 11:18:27 -0500

    *Update dist

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,5 @@
 import { Client, factory } from "@lumeweb/libkernel-universal";
-const MODULE = "AABpkxAEOLG5JKGVrC4pFCdFnBpA5SbBZYqtz_eIjEEabg";
+const MODULE = "AAAz2qQHwKQ7YurprbLmzpHc66BY0pljDZsa_a6QmeBe_g";
 export class HandshakeClient extends Client {
     async ready() {
         return this.callModuleReturn("ready");
