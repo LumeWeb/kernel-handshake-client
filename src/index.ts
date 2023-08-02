0ef0d1cb8e5ade43d62203a1bba776f16d111e68
Derrick Hammer <derrick@derrickhammer.com>
2023-08-02 17:37:14 -0400

    feat: add queryDns method

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,9 @@ export class HandshakeClient extends NetworkClient {
   public async query(method: string, params: any): Promise<Response> {
     return this.callModuleReturn("query", { method, params });
   }
+  public async queryDns(fqdn: string, type: string): Promise<Response> {
+    return this.callModuleReturn("queryDns", { fqdn, type });
+  }
 }
 
 export const createClient = factory<HandshakeClient>(HandshakeClient, MODULE);
